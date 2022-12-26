f22a147f6b132fa978915567ed7781b0e8abfbb3
BoloniniD <denis@bolonin.ru>
2022-12-26 18:08:16 +0300

    Hotfix: добавить проверку в бота

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import moex_api
+from datetime import datetime
 import sys
 
 if __name__ == "__main__":
-    trade = moex_api.Trades()
-    trade.process_request(sys.argv[1], sys.argv[2], sys.argv[3])
+    # trade = moex_api.Trades()
+
+    print(moex_api.check_date("2022-12-23", "SBER"))
+    # trade.process_request(sys.argv[1], sys.argv[2], sys.argv[3])
